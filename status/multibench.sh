fa07b77bea3b081415cb365a1b1369fd3c27958c
Masahiko Sawada <sawada.mshk@gmail.com>
2018-06-15 16:03:51 +0900

    add task

--- status/multibench.sh
+++ status/multibench.sh
@@ -1,3 +1,4 @@
-./dobench.sh -t master -T 5 -l 1 -i -C 32
-##./dobench -t ext -T 5 -l 1 -i -C 32
-
+./dobench.sh -t ext -S 10000 -T 180 -l 30 -i -C 32
+./dobench.sh -t ext -S 10000 -T 180 -l 30 -i -C 128
+./dobench.sh -t master -S 10000 -T 180 -l 30 -i -C 32
+./dobench.sh -t master -S 10000 -T 180 -l 30 -i -C 128
