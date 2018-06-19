c8e5cd7f015e7ee3c1ab2cad67c91e423166d5bf
Masahiko Sawada <sawada.mshk@gmail.com>
2018-06-19 20:25:03 +0900

    Update multibench.sh

--- status/multibench.sh
+++ status/multibench.sh
@@ -1,4 +1,9 @@
-./dobench.sh -t ext -S 100 -T 180 -l 30 -i -C 4 -c 8
-./dobench.sh -t ext -S 100 -T 180 -l 30 -i -C 4 -c 16
-./dobench.sh -t master -S 100 -T 180 -l 30 -i -C 4 -c 8
-./dobench.sh -t master -S 100 -T 180 -l 30 -i -C 4 -c 16
+./dobench.sh -t ext -S 10000 -T 180 -l 30 -i -C 4 -c 8
+./dobench.sh -t ext -S 10000 -T 180 -l 30 -i -C 4 -c 16
+./dobench.sh -t master -S 10000 -T 180 -l 30 -i -C 4 -c 8
+./dobench.sh -t master -S 10000 -T 180 -l 30 -i -C 4 -c 16
+
+./dobench.sh -t ext -S 10000 -T 180 -l 30 -i -C 16 -c 8
+./dobench.sh -t ext -S 10000 -T 180 -l 30 -i -C 16 -c 16
+./dobench.sh -t master -S 10000 -T 180 -l 30 -i -C 16 -c 8
+./dobench.sh -t master -S 10000 -T 180 -l 30 -i -C 16 -c 16
