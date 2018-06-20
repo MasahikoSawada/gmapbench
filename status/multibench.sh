b7092579581d98559b90dfc11e3fda6eb225bb28
Masahiko Sawada <sawada.mshk@gmail.com>
2018-06-20 16:30:18 +0900

    add task

--- status/multibench.sh
+++ status/multibench.sh
@@ -1,17 +1,18 @@
-./dobench.sh -t ext -S 100 -T 180 -l 30 -i -C 16 -c 16 -u
-./dobench.sh -t master -S 100 -T 180 -l 30 -i -C 16 -c 16 -u
+./dobench.sh -t ext -S 100 -T 180 -l 30 -i -C 16 -n 16 -u
+./dobench.sh -t master -S 100 -T 180 -l 30 -i -C 16 -n 16 -u
 
-./dobench.sh -t ext -S 100 -T 180 -l 30 -i -C 16 -c 32 -u
-./dobench.sh -t master -S 100 -T 180 -l 30 -i -C 16 -c 32 -u
+./dobench.sh -t ext -S 100 -T 180 -l 30 -i -C 16 -n 32 -u
+./dobench.sh -t master -S 100 -T 180 -l 30 -i -C 16 -n 32 -u
 
-./dobench.sh -t ext -S 100 -T 180 -l 30 -i -C 16 -c 16
-./dobench.sh -t master -S 100 -T 180 -l 30 -i -C 16 -c 16
+./dobench.sh -t ext -S 100 -T 180 -l 30 -i -C 16 -n 16
+./dobench.sh -t master -S 100 -T 180 -l 30 -i -C 16 -n 16
 
-./dobench.sh -t ext -S 100 -T 180 -l 30 -i -C 16 -c 32
-./dobench.sh -t master -S 100 -T 180 -l 30 -i -C 16 -c 32
+./dobench.sh -t ext -S 100 -T 180 -l 30 -i -C 16 -n 32
+./dobench.sh -t master -S 100 -T 180 -l 30 -i -C 16 -n 32
 
-./dobench.sh -t ext -S 10000 -T 180 -l 30 -i -C 16 -c 32 -u
-./dobench.sh -t master -S 10000 -T 180 -l 30 -i -C 16 -c 32 -u
+./dobench.sh -t ext -S 10000 -T 180 -l 30 -i -C 16 -n 32 -u
+./dobench.sh -t master -S 10000 -T 180 -l 30 -i -C 16 -n 32 -u
 
-./dobench.sh -t ext -S 10000 -T 180 -l 30 -i -C 32 -c 32 -u
-./dobench.sh -t master -S 10000 -T 180 -l 30 -i -C 32 -c 32 -u
+./dobench.sh -t ext -S 10000 -T 180 -l 30 -i -C 32 -n 32 -u
+./dobench.sh -t master -S 10000 -T 180 -l 30 -i -C 32 -n 32 -u
+
